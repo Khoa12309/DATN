d8ba03e194c3db3494ec2e41f5f9a1ff14792205
Xuân Khoa <vukhoa20122003@gmail.com>
2023-10-06 22:05:36 +0700

    GiaoDien admin

---
+++ APPVIEW/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APPVIEW.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
