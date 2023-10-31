48d0deaf1a00571f96c7dac23c79f90427cc3e19
Huong <huongpdph27798@fpt.edu.vn>
2023-10-31 22:05:12 +0700

    CRUD api, add view

---
+++ APPVIEW/Controllers/BillDetailController.cs
@@ -0,0 +1,75 @@
+﻿using APPDATA.Models;
+using APPVIEW.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APPVIEW.Controllers
+{
+    public class BillDetailController : Controller
+    {
+        private Getapi<BillDetail> getapi;
+        public BillDetailController()
+        {
+            getapi = new Getapi<BillDetail>();
+        }
+
+        public async Task<IActionResult> GetList()
+        {
+            var obj = getapi.GetApi("BillDetail");
+            return View(obj);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        // POST: SupplierController1/Create
+        [HttpPost]
+        public async Task<IActionResult> Create(BillDetail obj)
+        {
+            try
+            {
+                getapi.CreateObj(obj, "BillDetail");
+                return RedirectToAction("GetList");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+
+            var lst = getapi.GetApi("BillDetail");
+            return View(lst.Find(c => c.id == id));
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(BillDetail obj)
+        {
+            try
+            {
+                getapi.UpdateObj(obj, "BillDetail");
+                return RedirectToAction("GetList");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            getapi.DeleteObj(id, "BillDetail");
+            return RedirectToAction("GetList");
+
+        }
+    }
+}
---
+++ _APPAPI/Controllers/ImageController.cs
@@ -0,0 +1,49 @@
+﻿using _APPAPI.Service;
+using APPDATA.DB;
+using APPDATA.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _APPAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        ShoppingDB _context = new ShoppingDB();
+        private readonly CRUDapi<Image> _crud;
+        public ImageController()
+        {
+            _crud = new CRUDapi<Image>(_context, _context.Images);
+        }
+        [HttpGet]
+        public IEnumerable<Image> Getall()
+        {
+            return _crud.GetAllItems().ToList();
+        }
+        [Route("Post")]
+        [HttpPost]
+        public bool Creat(Image obj)
+        {
+            return _crud.CreateItem(obj);
+        }
+        [Route("Delete")]
+        [HttpDelete]
+        public bool Delete(Guid id)
+        {
+            Image item = _crud.GetAllItems().FirstOrDefault(c => c.Id == id);
+            return _crud.DeleteItem(item);
+        }
+        [Route("Update")]
+        [HttpPut]
+        public bool Update(Image obj)
+        {
+            Image item = _crud.GetAllItems().FirstOrDefault(c => c.Id == obj.Id);
+            item.Name = obj.Name;
+            item.Status = obj.Status;
+            item.Update_date = obj.Update_date;
+            item.Create_date = obj.Create_date;
+            return _crud.UpdateItem(item);
+        }
+    }
+}
