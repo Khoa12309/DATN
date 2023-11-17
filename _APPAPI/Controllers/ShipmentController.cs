de037bfd9f01089c8133129d8ede13c1df13f86b
Xuân Khoa <vukhoa20122003@gmail.com>
2023-11-17 22:14:52 +0700

    GHN

---
+++ _APPAPI/Controllers/ShipmentController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using _APPAPI.ViewModels;
+
+namespace _APPAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShipmentController : ControllerBase
+    {
+       
+        // Phương thức GET để trả về danh sách các quận/huyện
+        [HttpGet("districts")]
+        public async Task<IActionResult> GetDistricts(int provinceId)
+        {
+            // Tạo một đối tượng OnlineGatewayClient để gọi API
+            var client = new OnlineGatewayClient($"https://online-gateway.ghn.vn/shiip/public-api/master-data/province", "bdbbde2a-fec2-11ed-8a8c-6e4795e6d902");
+
+            // Gọi API [10] để lấy danh sách các quận/huyện theo tỉnh/thành phố
+            var response = await client.GetDistrictsAsync();
+
+            // Kiểm tra kết quả trả về
+            //if (response.Code == 1) // Thành công
+            //{
+            //    // Trả về danh sách các quận/huyện dưới dạng JSON
+            //    return Ok(response.Data);
+            //}
+            //else // Thất bại
+            //{
+            //    // Trả về thông báo lỗi
+            //    return BadRequest(response.Message);
+            //}
+            return BadRequest("adsads");
+        }
+
+      
+    }
+}
---
+++ _APPAPI/ViewModels/Province.cs
@@ -0,0 +1,24 @@
+﻿namespace _APPAPI.ViewModels
+{
+    public class Province
+    {
+       public int ProvinceID { get; set; }
+       public string ProvinceName { get; set; }
+       public int CountryID { get; set; }
+       public string Code { get; set; }
+       public string[] NameExtension { get; set; }
+
+
+           
+           
+          
+            //"IsEnable": 1,
+            //"RegionID": 6,
+            //"RegionCPN": 5,
+            //"UpdatedBy": 1718600,
+            //"CreatedAt": "2019-12-05 15:41:26.891384 +0700 +07 m=+0.010448463",
+            //"UpdatedAt": "2019-12-05 15:41:26.891384 +0700 +07 m=+0.010449016",
+            //"CanUpdateCOD": false,
+            //"Status": 1
+    }
+}
