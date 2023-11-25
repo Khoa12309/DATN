d173f2e137f6b7db7ff7ad13651b5157f203125d
Huong <huongpdph27798@fpt.edu.vn>
2023-11-25 21:14:57 +0700

    thống kê theo ngày

--- _APPAPI/Controllers/ChartController.cs
+++ _APPAPI/Controllers/ChartController.cs
@@ -10,15 +10,12 @@ namespace _APPAPI.Controllers
     public class ChartController : ControllerBase
     {
         private readonly ShoppingDB _context;
-        //private readonly CRUDapi<Bill> _crud;
 
-        //private readonly IConfiguration _Configuration;
         public ChartController()
         {
-            //_crud = new CRUDapi<Bill>(_context, _context.Bills);
-            //_Configuration = configuration;
             _context = new ShoppingDB(new DbContextOptions<ShoppingDB>());
         }
+
         [HttpGet("{year}")]
         public IActionResult StatisticsByYear(int year)
         {
@@ -26,17 +23,59 @@ namespace _APPAPI.Controllers
             {
                 var data = _context.Bills
                     .Where(b => b.PayDate.HasValue && b.PayDate.Value.Year == year)
-                    .OrderBy(b => b.PayDate)
-                    .Select(b => new { Month = b.PayDate.Value.Month, Money = b.TotalMoney })
+                    .GroupBy(b => b.PayDate.Value.Month)  // Nhóm hóa đơn theo tháng
+                    .Select(group => new
+                    {
+                        Month = group.Key,
+                        Money = group.Sum(b => b.TotalMoney)
+                    })
+                    .OrderBy(item => item.Month)
                     .ToList();
 
                 // Chuyển đổi dữ liệu thành định dạng phù hợp cho biểu đồ
                 var chartData = new
                 {
-                    labels = data.Select(bill => bill.Month),
-                    values = data.Select(bill => bill.Money),
+                    labels = data.Select(item => item.Month),
+                    values = data.Select(item => item.Money),
                     label = "Doanh Thu"
                 };
+
+                return Ok(chartData);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { error = ex.Message });
+            }
+        }
+        // API để vẽ biểu đồ thống kê doanh thu trong 1 tuần qua
+        [HttpGet("weekly")]
+        public IActionResult WeeklyStatistics()
+        {
+            try
+            {
+                // Lấy ngày hiện tại
+                DateTime currentDate = DateTime.Now;
+
+                // Lấy dữ liệu từ database cho doanh thu trong 1 tuần qua
+                var data = _context.Bills
+                    .Where(b => b.PayDate.HasValue && b.PayDate.Value >= currentDate.AddDays(-7))
+                    .GroupBy(b => b.PayDate.Value.Date)  // Nhóm theo ngày
+                    .OrderBy(group => group.Key)
+                    .Select(group => new
+                    {
+                        Date = group.Key,
+                        Money = group.Sum(b => b.TotalMoney)
+                    })
+                    .ToList();
+
+                // Chuyển đổi dữ liệu thành định dạng phù hợp cho biểu đồ
+                var chartData = new
+                {
+                    labels = data.Select(item => item.Date.ToShortDateString()), // Format ngày thành chuỗi
+                    values = data.Select(item => item.Money),
+                    label = "Doanh Thu Tuần Qua"
+                };
+
                 return Ok(chartData);
             }
             catch (Exception ex)
