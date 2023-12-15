715bf2e152346e649720b0e98f5aa20794e059c1
Xuân Khoa <vukhoa20122003@gmail.com>
2023-12-15 15:24:25 +0700

    masstre

--- APPVIEW/Controllers/ColorController.cs
+++ APPVIEW/Controllers/ColorController.cs
@@ -120,6 +120,7 @@ namespace APPVIEW.Controllers
                
                 if (await getapi.DeleteObj(id, "Color"))
                 {
+                    _notyf.Success("Chuyển trạng thái thành công");
                     var lst = getapi.GetApi("Color").Find(c => c.Id == id);
                     lst.Status = 0;
                     await getapi.UpdateObj(lst, "Color");
